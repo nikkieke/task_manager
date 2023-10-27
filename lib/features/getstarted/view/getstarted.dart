42308c129d07020c94c74c09a2b2c1e982889fea
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-10-27 20:50:49 +0100

    boilerplate code

--- lib/features/getstarted/view/getstarted.dart
+++ lib/features/getstarted/view/getstarted.dart
@@ -1,16 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
 
-class GetStartedView extends StatelessWidget {
+class GetStartedView extends StatefulWidget {
   const GetStartedView({super.key});
+
+  @override
+  State<GetStartedView> createState() => _GetStartedViewState();
+}
+
+class _GetStartedViewState extends State<GetStartedView> {
+  late RiveAnimationController<dynamic> animationBtnController;
+
+  @override
+  void initState() {
+    animationBtnController = OneShotAnimation(
+      'Timeline 1',
+      autoplay: false,
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Text('Manage your Tasks with Task Manager', style: Theme.of(context).textTheme.titleLarge,),
-          ],
+        child:
+        // Column(
+        //   children: [
+        //     Text('Manage your Tasks with Task Manager', style: Theme.of(context).textTheme.titleLarge,),
+        //   ],
+        // ),
+        Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Center(
+            child: GestureDetector(
+              onTap: (){
+                animationBtnController.isActive = true;
+              },
+              child: SizedBox(
+                width: 300,
+                height: 58,
+                child: RiveAnimation.asset(
+                  'assets/images/button.riv',
+                  controllers: [animationBtnController],
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
