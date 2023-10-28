1002fdbb08b6b7ff9e17de43e4615e347685feaf
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-10-28 18:24:43 +0100

    add app image file

---
+++ lib/app/utils/helpers/app_image/app_image.dart
@@ -0,0 +1,7 @@
+class AppImage{
+  static const pomodoro = 'assets/images/pomodoro.riv';
+  static const eyeSlash = 'assets/images/eyeslash.svg';
+  static const lock = 'assets/images/lock.svg';
+  static const user = 'assets/images/user.svg';
+  static const userTag = 'assets/images/usertag.svg';
+}
