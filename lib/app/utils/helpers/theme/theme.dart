39329545a245883a6b4b81aa2940db9f5990a254
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-10-28 18:30:03 +0100

    update app theme

--- lib/app/utils/helpers/theme/theme.dart
+++ lib/app/utils/helpers/theme/theme.dart
@@ -4,15 +4,17 @@
 // #141718
 // #232627
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme{
   static  ThemeData get dark {
     return ThemeData(
       useMaterial3: true,
-      primaryColor: const Color(0xfffed36a),
+      primaryColor: const Color(0xffb42c3a),
       scaffoldBackgroundColor: const Color(0xff141718),
       elevatedButtonTheme: elevatedButtonThemeData,
+      outlinedButtonTheme: outlinedButtonThemeData,
       textTheme: textTheme(),
 
 
@@ -34,7 +36,8 @@ class AppTheme{
   static ElevatedButtonThemeData get elevatedButtonThemeData {
     return ElevatedButtonThemeData(
       style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all<Color>(const Color(0xfffed36a)),
+        backgroundColor: MaterialStateProperty.all<Color>(const Color(0xffb42c3a),),
+        minimumSize: MaterialStateProperty.all( Size(376.w, 70.h)),
         shape: MaterialStateProperty.all<OutlinedBorder>(
           RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10), // Border radius
@@ -43,4 +46,21 @@ class AppTheme{
       ),
     );
   }
+
+  static OutlinedButtonThemeData get outlinedButtonThemeData {
+    return OutlinedButtonThemeData(
+        style: ButtonStyle(
+          side: MaterialStateProperty.all(const BorderSide(
+            color: Color(0xFFa6a6a7),         // Border width
+          ),),
+            minimumSize: MaterialStateProperty.all( Size(376.w, 70.h)),
+            shape: MaterialStateProperty.all<OutlinedBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10), // Border radius
+              ),
+            ),
+        ),
+    );
+  }
+
 }
