42b87ae92ad5c8d7a6079f5e3d49b7d4afdf6698
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-10-30 23:22:55 +0100

    update features

--- lib/features/features.dart
+++ lib/features/features.dart
@@ -1,3 +1,6 @@
 export 'auth/auth.dart';
+export 'calendar/calendar.dart';
+export 'chat/chat.dart';
 export 'getstarted/getstarted.dart';
 export 'home/home.dart';
+export 'new_project/new_project.dart';
