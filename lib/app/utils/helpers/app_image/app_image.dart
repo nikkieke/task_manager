d725482a4831811e51821cad4fdb0b3f2f8e8ace
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-10-31 23:14:46 +0100

    update appImage constants

--- lib/app/utils/helpers/app_image/app_image.dart
+++ lib/app/utils/helpers/app_image/app_image.dart
@@ -4,4 +4,22 @@ class AppImage{
   static const lock = 'assets/images/lock.svg';
   static const user = 'assets/images/user.svg';
   static const userTag = 'assets/images/usertag.svg';
+  static const notification = 'assets/images/notification.svg';
+  static const setting = 'assets/images/setting.svg';
+  static const messages = 'assets/images/messages.svg';
+  static const home = 'assets/images/home.svg';
+  static const calendar = 'assets/images/calendar.svg';
+  static const addSquare = 'assets/images/addsquare.svg';
+  static const google = 'assets/images/google.svg';
+  static const edit = 'assets/images/edit.svg';
+  static const calendarRemove = 'assets/images/calendarremove.svg';
+  static const arrowLeft = 'assets/images/arrowleft.svg';
+  static const users = 'assets/images/users.svg';
+  static const avatar1 = 'assets/images/avatar1.png';
+  static const avatar2 = 'assets/images/avatar2.png';
+  static const avatar3 = 'assets/images/avatar3.png';
+  static const avatar4 = 'assets/images/avatar4.png';
+  static const avatar5 = 'assets/images/avatar5.png';
+  static const avatar6 = 'assets/images/avatar6.png';
+  static const avatar7 = 'assets/images/avatar7.png';
 }
