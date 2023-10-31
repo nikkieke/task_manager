20d707185df9c345951c78d9899e703b116d80ec
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-10-31 23:16:42 +0100

    update router

--- lib/app/utils/helpers/routing/go_router_provider.dart
+++ lib/app/utils/helpers/routing/go_router_provider.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:task_manager/features/features.dart';
+import 'package:task_manager/features/notifications/notifications.dart';
 
 final GlobalKey<NavigatorState> _rootNavigation = GlobalKey(debugLabel: 'root');
+final GlobalKey<NavigatorState> _shellNavigation = GlobalKey(debugLabel: 'shell');
 
 final goRouterProvider = Provider<GoRouter>((ref){
   return GoRouter(
@@ -33,10 +35,59 @@ final goRouterProvider = Provider<GoRouter>((ref){
         builder: (context, state) => const ForgotPasswordView(),
       ),
 
-      GoRoute(
-        path: '/home',
-        name: AppRoute.home.name,
-        builder: (context, state) => const HomeView(),
+      ShellRoute(
+        navigatorKey: _shellNavigation,
+        parentNavigatorKey: _rootNavigation,
+        restorationScopeId: 'app',
+        builder: (context, state, child)=> MainScreen(key: state.pageKey,child: child,),
+          routes: [
+            GoRoute(
+              path: '/home',
+              name: AppRoute.home.name,
+              pageBuilder: (context, state)=>  NoTransitionPage(
+                  child: HomeView(key: state.pageKey,),
+              ),
+              routes: [
+                GoRoute(
+                  parentNavigatorKey: _rootNavigation,
+                  path: 'projectDetails',
+                  name: AppRoute.projectDetails.name,
+                  pageBuilder: (context, state)=>  NoTransitionPage(
+                    child: ProjectDetailsView(key: state.pageKey,),
+                  ),
+                ),
+
+              ],
+            ),
+            GoRoute(
+              path: '/chat',
+              name: AppRoute.chat.name,
+              pageBuilder: (context, state)=>  NoTransitionPage(
+                child: ChatView(key: state.pageKey,),
+              ),
+            ),
+            GoRoute(
+              path: '/newProject',
+              name: AppRoute.newProject.name,
+              pageBuilder: (context, state)=>  NoTransitionPage(
+                child: NewProjectView(key: state.pageKey,),
+              ),
+            ),
+            GoRoute(
+              path: '/calendar',
+              name: AppRoute.calendar.name,
+              pageBuilder: (context, state)=>  NoTransitionPage(
+                child: CalendarView(key: state.pageKey,),
+              ),
+            ),
+            GoRoute(
+              path: '/notifications',
+              name: AppRoute.notifications.name,
+              pageBuilder: (context, state)=>  NoTransitionPage(
+                child: NotificationsView(key: state.pageKey,),
+              ),
+            ),
+          ],
       ),
     ],
   );
@@ -48,4 +99,9 @@ enum AppRoute {
   signup,
   forgotPassword,
   home,
+  chat,
+  newProject,
+  calendar,
+  notifications,
+  projectDetails,
 }
