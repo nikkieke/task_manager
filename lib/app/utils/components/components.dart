8a9eb7c715278246730421d3c07d0ed809bad3a7
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-11-12 00:16:20 +0100

    export path to indicator widget

--- lib/app/utils/components/components.dart
+++ lib/app/utils/components/components.dart
@@ -1,3 +1,4 @@
 export 'app_spacer/app_spacer.dart';
 export 'progress_indicator/progress_indicator.dart';
+export 'pull_to_refresh/pull_to_refresh.dart';
 export 'textform_input/textform_input.dart';
