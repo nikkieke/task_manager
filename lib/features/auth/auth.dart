36c203f7e754fc19bfd20359646281d58eece59b
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-12-03 06:41:52 +0100

    add auth functions

--- lib/features/auth/auth.dart
+++ lib/features/auth/auth.dart
@@ -1,3 +1,4 @@
 export 'forgot_password/forgot_password.dart';
 export 'login/login.dart';
+export 'service/service.dart';
 export 'signup/signup.dart';
--- lib/features/auth/service/service.dart
+++ lib/features/auth/service/service.dart
@@ -1,2 +1,2 @@
 export 'package:task_manager/features/auth/service/auth_service.dart';
-export 'package:task_manager/features/auth/service/handlers/handlers';
+export 'package:task_manager/features/auth/service/handlers/handlers.dart';
