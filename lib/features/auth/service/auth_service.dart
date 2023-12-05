071ac657b8341052724468b95639c405c61de162
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-12-05 22:32:07 +0100

    auth functions

--- lib/features/auth/service/auth_service.dart
+++ lib/features/auth/service/auth_service.dart
@@ -21,14 +21,20 @@ class AuthService{
 
   Future<Either<ErrorHandler, SuccessHandler<User>>> login(String email, String password)async{
       try{
+        /// using the signInWithEmailAndPassword method to get the signin response
             final authResult = await firebaseAuth.signInWithEmailAndPassword(
               email: email, password: password,);
+              /// if the user object in the signin response is not null
+              /// return the SuccessHandler with the user object
               if(authResult.user != null){
                 return Right(SuccessHandler(authResult.user!));
               }else {
+                ///Else return a signin error
                 return const Left(ErrorHandler('Could not get user'));
               }
       }on FirebaseAuthException catch (e){
+        ///Also catch the firebase exception error message and code through
+        ///the error handler
         return Left(ErrorHandler(e.message?? '', code: e.code));
       }
   }
@@ -55,5 +61,54 @@ Future<Either<ErrorHandler, bool>> sendResetEmail(String email)async{
         return Left(ErrorHandler(e.message?? '', code: e.code));
       }
 }
+
+Future<Either<ErrorHandler, SuccessHandler<User>>> changePassword(
+  String email, String oldPw, String newPw,
+)async{
+  try{
+    final logIn = await login(email, oldPw);
+    if(logIn.isRight){
+      final user = logIn.right.data;
+      await user.updatePassword(newPw);
+      return Right(SuccessHandler(user));
+    }else{
+      final error = logIn.left;
+      return Left(ErrorHandler(error.message, code: error.code));
+    }
+  }on FirebaseAuthException catch(e){
+    return Left(ErrorHandler(e.message?? '', code: e.code));
+  }
+
+}
+
+Future<Either<ErrorHandler, String>> sendResetPasswordLink(String email)async{
+  try{
+    await firebaseAuth.sendPasswordResetEmail(email: email);
+    return Right('A mail containing the link to reset your password has been sent to $email');
+  }on FirebaseAuthException catch(error){
+    return Left(ErrorHandler(error.message?? '', code: error.code));
+  }catch (e){
+    return const Left(ErrorHandler("Oops! Something went wrong and your request couldn't be completed", 
+    code: '500',),);
+  }
+}
+
+Future<Either<ErrorHandler, bool>> signOut() async{
+  try{
+    await firebaseAuth.signOut();
+    return const Right(true);
+  }on FirebaseAuthException catch(e){
+    return Left(ErrorHandler(e.message?? '', code: e.code));
+  }
+}
+
+Future<void> deleteCurrentFirebaseUser()async{
+  if(firebaseAuth.currentUser != null){
+    await firebaseAuth.currentUser?.delete();
+    await signOut();
+  }
+}
+
+
 
 }
