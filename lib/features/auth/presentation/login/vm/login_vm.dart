71a9292515947f3d730d544119b9112fd9612f6b
nikkieke <nikkieke001@gmail.com>
2024-06-20 04:30:09 +0100

    chore: update function

--- lib/features/auth/presentation/login/vm/login_vm.dart
+++ lib/features/auth/presentation/login/vm/login_vm.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:task_manager/app/app.dart';
 import 'package:task_manager/features/features.dart';
 
 final logInProvider = StateNotifierProvider.autoDispose<LogInNotifier,
