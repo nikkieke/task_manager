a473d6073a71f92418efc7e8ef04d0118bf75cf8
nikkieke <nikkieke001@gmail.com>
2024-06-20 04:32:36 +0100

    chore: add http service

---
+++ lib/app/http/dio_http_service.dart
@@ -0,0 +1,83 @@
+import 'package:dio/dio.dart';
+import 'package:task_manager/app/app.dart';
+
+
+class DioService implements HttpService {
+
+  factory DioService() => _instance;
+
+
+  DioService._({
+    Dio? dioOverride,
+  }) {
+    //_dio = Dio(baseOptions);
+    _dio = dioOverride ?? Dio(baseOptions);
+      _dio.interceptors.addAll([
+
+      ]);
+  }
+
+  late final Dio _dio;
+  static final DioService _instance = DioService._();
+
+
+  BaseOptions get baseOptions => BaseOptions(
+    connectTimeout: const Duration(seconds: 200),
+    receiveTimeout: const Duration(seconds: 200),
+    baseUrl: baseUrl,
+    headers: headers,
+    validateStatus: (_)=> true,
+  );
+
+  @override
+  String get baseUrl => AppConfig.baseUrl;
+
+  @override
+  Map<String, String> get headers => {
+    'accept': 'application/json',
+    'content-type': 'application/json',
+  };
+
+  @override
+  Future<Response<dynamic>> request(String path, RequestMethod method,
+      {dynamic savePath,
+        Map<String, dynamic>? queryParams,
+        dynamic data,
+        Options? options,}) async {
+    Response<dynamic> response;
+
+    try {
+      switch (method) {
+        case RequestMethod.post:
+          response = await _dio.post(
+            path,
+            queryParameters: queryParams,
+            data: data,
+          );
+        case RequestMethod.get:
+          response = await _dio.get(
+            path,
+            options: options,
+            queryParameters: queryParams,
+          );
+
+        case RequestMethod.put:
+          response = await _dio.put(
+            path,
+            queryParameters: queryParams,
+            data: data,
+          );
+        case RequestMethod.delete:
+          response = await _dio.delete(
+            path,
+            data: data,
+          );
+      }
+      return response;
+    } on DioException catch (e) {
+      BaseUtils.basicPrint(e.toString());
+      final errorMessage = DioErrorHandler.fromDioError(e).toString();
+      throw errorMessage;
+    }
+  }
+}
