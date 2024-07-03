4da5d6164207a791565f90126083282922754dad
nikkieke <nikkieke001@gmail.com>
2024-07-03 09:06:07 +0100

    ⚒ refactor function names

--- lib/features/home/domain/user_repo.dart
+++ lib/features/home/domain/user_repo.dart
@@ -1,21 +1,21 @@
 import 'package:either_dart/either.dart';
 import 'package:task_manager/features/features.dart';
 
-abstract class UserRepository{
-  Future<Either<ErrorHandler, NewUser>>saveUserInfo();
-  Future<Either<ErrorHandler, NewUser>>getUserInfo(String key);
+abstract class UserRepository {
+  Future<Either<ErrorHandler, NewUser>> getUserFromFireStore();
+  Future<Either<ErrorHandler, NewUser>> getStoredUser(String key);
 }
 
-class UserRepoImpl implements UserRepository{
+class UserRepoImpl implements UserRepository {
   final _userService = UserService.instance;
 
   @override
-  Future<Either<ErrorHandler, NewUser>> saveUserInfo() {
-    return _userService.saveUserInfo();
+  Future<Either<ErrorHandler, NewUser>> getUserFromFireStore() {
+    return _userService.getUserFromFireStore();
   }
 
   @override
-  Future<Either<ErrorHandler, NewUser>> getUserInfo(String key) {
-    return _userService.getUserInfo(key);
+  Future<Either<ErrorHandler, NewUser>> getStoredUser(String key) {
+    return _userService.getStoredUser(key);
   }
 }
