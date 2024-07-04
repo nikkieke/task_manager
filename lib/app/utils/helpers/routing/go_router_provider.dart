9b17d4bad0162b680130122b83d6f114b21f7410
nikkieke <nikkieke001@gmail.com>
2024-07-04 10:45:29 +0100

    🪡 add todo

--- lib/app/utils/helpers/routing/go_router_provider.dart
+++ lib/app/utils/helpers/routing/go_router_provider.dart
@@ -3,11 +3,11 @@ import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:task_manager/features/features.dart';
 
-
 final GlobalKey<NavigatorState> rootNavigation = GlobalKey(debugLabel: 'root');
-final GlobalKey<NavigatorState> _shellNavigation = GlobalKey(debugLabel: 'shell');
+final GlobalKey<NavigatorState> _shellNavigation =
+    GlobalKey(debugLabel: 'shell');
 
-final goRouterProvider = Provider<GoRouter>((ref){
+final goRouterProvider = Provider<GoRouter>((ref) {
   final analytics = ref.watch(analyticsProvider);
   return GoRouter(
     initialLocation: '/',
@@ -19,6 +19,7 @@ final goRouterProvider = Provider<GoRouter>((ref){
     ],
     redirect: (context, state) {
       final isFirstLaunch = SharedPrefManager.isFirstLaunch;
+      //TODO confirm if user has verified email before getting to the home page
       if (isFirstLaunch) {
         if (state.fullPath == '/') {
           return '/';
@@ -52,95 +53,112 @@ final goRouterProvider = Provider<GoRouter>((ref){
         name: AppRoute.forgotPassword.name,
         builder: (context, state) => const ForgotPasswordView(),
       ),
-
       GoRoute(
         path: '/verifyEmail',
         name: AppRoute.verifyEmail.name,
         builder: (context, state) => const VerifyEmailView(),
       ),
-
       ShellRoute(
         navigatorKey: _shellNavigation,
         parentNavigatorKey: rootNavigation,
         restorationScopeId: 'app',
-        builder: (context, state, child)=> MainScreen(key: state.pageKey,child: child,),
-          routes: [
-            GoRoute(
-              path: '/home',
-              name: AppRoute.home.name,
-              pageBuilder: (context, state)=>  NoTransitionPage(
-                  child: HomeView(key: state.pageKey,),
+        builder: (context, state, child) => MainScreen(
+          key: state.pageKey,
+          child: child,
+        ),
+        routes: [
+          GoRoute(
+            path: '/home',
+            name: AppRoute.home.name,
+            pageBuilder: (context, state) => NoTransitionPage(
+              child: HomeView(
+                key: state.pageKey,
               ),
-              routes: [
-                GoRoute(
-                  parentNavigatorKey: rootNavigation,
-                  path: 'projectDetails',
-                  name: AppRoute.projectDetails.name,
-                  pageBuilder: (context, state)=>  NoTransitionPage(
-                    child: ProjectDetailsView(key: state.pageKey,),
+            ),
+            routes: [
+              GoRoute(
+                parentNavigatorKey: rootNavigation,
+                path: 'projectDetails',
+                name: AppRoute.projectDetails.name,
+                pageBuilder: (context, state) => NoTransitionPage(
+                  child: ProjectDetailsView(
+                    key: state.pageKey,
                   ),
-                  routes: [
-                    GoRoute(
-                      parentNavigatorKey: rootNavigation,
-                      path: 'addTask',
-                      name: AppRoute.addTask.name,
-                      pageBuilder: (context, state)=>  NoTransitionPage(
-                        child: AddTaskView(key: state.pageKey,),
+                ),
+                routes: [
+                  GoRoute(
+                    parentNavigatorKey: rootNavigation,
+                    path: 'addTask',
+                    name: AppRoute.addTask.name,
+                    pageBuilder: (context, state) => NoTransitionPage(
+                      child: AddTaskView(
+                        key: state.pageKey,
                       ),
                     ),
-                  ],
-                ),
-                GoRoute(
-                  parentNavigatorKey: rootNavigation,
-                  path: 'profile',
-                  name: AppRoute.profile.name,
-                  pageBuilder: (context, state)=>  NoTransitionPage(
-                    child: ProfileView(key: state.pageKey,),
+                  ),
+                ],
+              ),
+              GoRoute(
+                parentNavigatorKey: rootNavigation,
+                path: 'profile',
+                name: AppRoute.profile.name,
+                pageBuilder: (context, state) => NoTransitionPage(
+                  child: ProfileView(
+                    key: state.pageKey,
                   ),
                 ),
-
-              ],
-            ),
-            GoRoute(
-              path: '/chat',
-              name: AppRoute.chat.name,
-              pageBuilder: (context, state)=>  NoTransitionPage(
-                child: ChatView(key: state.pageKey,),
               ),
-              routes: [
-                GoRoute(
-                  parentNavigatorKey: rootNavigation,
-                  path: 'newChat',
-                  name: AppRoute.newChat.name,
-                  pageBuilder: (context, state)=>  NoTransitionPage(
-                    child: NewChatView(key: state.pageKey,),
+            ],
+          ),
+          GoRoute(
+            path: '/chat',
+            name: AppRoute.chat.name,
+            pageBuilder: (context, state) => NoTransitionPage(
+              child: ChatView(
+                key: state.pageKey,
+              ),
+            ),
+            routes: [
+              GoRoute(
+                parentNavigatorKey: rootNavigation,
+                path: 'newChat',
+                name: AppRoute.newChat.name,
+                pageBuilder: (context, state) => NoTransitionPage(
+                  child: NewChatView(
+                    key: state.pageKey,
                   ),
                 ),
-
-              ],
-            ),
-            GoRoute(
-              path: '/newProject',
-              name: AppRoute.newProject.name,
-              pageBuilder: (context, state)=>  NoTransitionPage(
-                child: NewProjectView(key: state.pageKey,),
+              ),
+            ],
+          ),
+          GoRoute(
+            path: '/newProject',
+            name: AppRoute.newProject.name,
+            pageBuilder: (context, state) => NoTransitionPage(
+              child: NewProjectView(
+                key: state.pageKey,
               ),
             ),
-            GoRoute(
-              path: '/calendar',
-              name: AppRoute.calendar.name,
-              pageBuilder: (context, state)=>  NoTransitionPage(
-                child: CalendarView(key: state.pageKey,),
+          ),
+          GoRoute(
+            path: '/calendar',
+            name: AppRoute.calendar.name,
+            pageBuilder: (context, state) => NoTransitionPage(
+              child: CalendarView(
+                key: state.pageKey,
               ),
             ),
-            GoRoute(
-              path: '/notifications',
-              name: AppRoute.notifications.name,
-              pageBuilder: (context, state)=>  NoTransitionPage(
-                child: NotificationsView(key: state.pageKey,),
+          ),
+          GoRoute(
+            path: '/notifications',
+            name: AppRoute.notifications.name,
+            pageBuilder: (context, state) => NoTransitionPage(
+              child: NotificationsView(
+                key: state.pageKey,
               ),
             ),
-          ],
+          ),
+        ],
       ),
     ],
   );
