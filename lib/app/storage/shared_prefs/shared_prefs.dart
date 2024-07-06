f20752e1ecc3b70e8fe38b92ef23c151264a337a
nikkieke <nikkieke001@gmail.com>
2024-07-06 01:16:41 +0100

    update shared pref

--- lib/app/storage/shared_prefs/shared_prefs.dart
+++ lib/app/storage/shared_prefs/shared_prefs.dart
@@ -1,24 +1,27 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-class SharedPrefManager{
+class SharedPrefManager {
   static late SharedPreferences prefs;
 
   //init shared preferences
-  static Future<void> init()async{
+  static Future<void> init() async {
     prefs = await SharedPreferences.getInstance();
   }
 
-  static set isFirstLaunch(bool isFirstLaunch)=>
+  static set isFirstLaunch(bool isFirstLaunch) =>
       prefs.setBool('isFirstLaunch', isFirstLaunch);
   static bool get isFirstLaunch => prefs.getBool('isFirstLaunch') ?? true;
 
-  static set isLoggedIn(bool isLoggedIn)=>
+  static set isLoggedIn(bool isLoggedIn) =>
       prefs.setBool('isLoggedIn', isLoggedIn);
-  static bool get isLoggedIn => prefs.getBool('isLoggedIn')?? true;
+  static bool get isLoggedIn => prefs.getBool('isLoggedIn') ?? true;
+
+  static set isEmailVerified(bool isEmailVerified) =>
+      prefs.setBool('isEmailVerified', isEmailVerified);
+  static bool get isEmailVerified => prefs.getBool('isEmailVerified') ?? true;
 
   static void clear() {
     prefs.clear();
     //SharedPrefManager.isFirstLaunch = false;
   }
-
 }
