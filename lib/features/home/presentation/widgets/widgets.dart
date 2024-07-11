27f155072084c07b328a372846060ffdf926779d
nikkieke <nikkieke001@gmail.com>
2024-07-11 08:54:01 +0100

    update path

--- lib/features/home/presentation/widgets/widgets.dart
+++ lib/features/home/presentation/widgets/widgets.dart
@@ -4,3 +4,5 @@ export 'all_tasks_card.dart';
 export 'completed_projects_card.dart';
 export 'ongoing_projects.dart';
 export 'ongoing_projects_card.dart';
+export 'search_bar.dart';
+export 'welcome_user.dart';
