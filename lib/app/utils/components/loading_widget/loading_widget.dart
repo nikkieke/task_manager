9ce83136c5ede6cf872287317faf19b4e1ca95d1
nikkieke <nikkieke001@gmail.com>
2024-07-16 12:31:24 +0100

    ⏫ update loading widget

--- lib/app/utils/components/loading_widget/loading_widget.dart
+++ lib/app/utils/components/loading_widget/loading_widget.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
-//Todo fix loading widget
 Widget userNameLoading() {
-  return Skeletonizer(
+  return const Skeletonizer(
     effect: ShimmerEffect(),
-    child: Container(
-      height: 20,
-      width: 100,
-      child: const Text(
-        'Username',
-        style: TextStyle(
-          fontSize: 18,
-          color: Color(0xFFa6a6a7),
-        ),
+    containersColor: Colors.black,
+    child: Text(
+      'Username',
+      style: TextStyle(
+        fontSize: 18,
       ),
     ),
   );
