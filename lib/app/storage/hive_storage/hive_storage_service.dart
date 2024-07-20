e3a5bc7534abfdff683d608afc3759328ab0d6f0
nikkieke <nikkieke001@gmail.com>
2024-07-21 00:30:21 +0100

    🔧 refactor

--- lib/app/storage/hive_storage/hive_storage_service.dart
+++ lib/app/storage/hive_storage/hive_storage_service.dart
@@ -1,16 +1,17 @@
 import 'package:hive_flutter/hive_flutter.dart';
 
-class HiveStorageService{
+class HiveStorageService {
   HiveStorageService._privateConstructor();
 
-  static final HiveStorageService _instance = HiveStorageService._privateConstructor();
+  static final HiveStorageService _instance =
+      HiveStorageService._privateConstructor();
   static HiveStorageService get instance => _instance;
 
   //define a hive box
-  late Box<dynamic>hiveBox;
+  late Box<dynamic> hiveBox;
 
   //Opens hive box by it's name
-  Future<void>openHiveBox(String boxName)async{
+  Future<void> openHiveBox(String boxName) async {
     hiveBox = await Hive.openBox<dynamic>(boxName);
   }
 
@@ -18,19 +19,17 @@ class HiveStorageService{
     hiveBox = await Hive.openBox<dynamic>(boxName);
   }
 
-  Future<void> init() async{
+  Future<void> init() async {
     await openAppBox();
   }
 
-
-  Future<void> clear() async{
+  Future<void> clear() async {
     await hiveBox.clear();
   }
 
-  Future<void> close() async{
+  Future<void> close() async {
     await hiveBox.close();
   }
-
 
   dynamic get(String key) {
     return hiveBox.get(key);
@@ -40,27 +39,25 @@ class HiveStorageService{
     return hiveBox.values.toList();
   }
 
-
   bool has(String key) {
     return hiveBox.containsKey(key);
   }
 
-  Future<void> remove(String key) async{
+  Future<void> remove(String key) async {
     await hiveBox.delete(key);
   }
 
-  Future<void> set(String? key, dynamic data) async{
+  Future<void> set(String? key, dynamic data) async {
     await hiveBox.put(key, data);
   }
 
-  dynamic add(List<dynamic> value) async{
+  dynamic add(List<dynamic> value) async {
     await hiveBox.add(value);
   }
-
 }
 
-enum StorageKey{
+enum StorageKey {
   firebaseIdToken,
-  userprofile,
+  userProfile,
   userEmail,
 }
