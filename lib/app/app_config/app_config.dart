30ae5dc6e8da61dba74778ef8d012c76f050b4e4
nikkieke <nikkieke001@gmail.com>
2024-07-30 13:43:17 +0100

    🔧 refactor: app url

--- lib/app/app_config/app_config.dart
+++ lib/app/app_config/app_config.dart
@@ -1,31 +1,29 @@
 import 'package:task_manager/app/app.dart';
 
-enum Flavor{dev, prod}
+enum Flavor { dev, prod }
 
-class AppConfig{
-
-  AppConfig._privateConstructor(){
-      if(_flavor == null){
-        BaseUtils.log(
-            'AppConfig',
-            'Init has not been called',
-          message: 'Call init to set the flavor, flavor set to dev by default',
-        );
-        _flavor = Flavor.dev;
-      }
+class AppConfig {
+  AppConfig._privateConstructor() {
+    if (_flavor == null) {
+      BaseUtils.log(
+        'AppConfig',
+        'Init has not been called',
+        message: 'Call init to set the flavor, flavor set to dev by default',
+      );
+      _flavor = Flavor.dev;
+    }
   }
   static final AppConfig _instance = AppConfig._privateConstructor();
   static AppConfig get instance => _instance;
   static Flavor? _flavor;
 
   //set Flavor
-  static void init(Flavor flavor){
+  static void init(Flavor flavor) {
     _flavor = flavor;
   }
 
-  static const baseUrl = 'https://taskmanager-icd4.onrender.com/api/';
+  static const baseUrl = 'https://taskmanager-icd4.onrender.com/api';
 
   // verify email
-String get verifyEmail => '/emailVerify/';
-
+  String get verifyEmail => '/emailVerify';
 }
