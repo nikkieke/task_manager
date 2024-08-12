0e5032abc8c01820e82ec5db733eaa543d5536bd
nikkieke <nikkieke001@gmail.com>
2024-08-13 00:50:46 +0100

    💡 feat: update logout impl

--- lib/features/home/presentation/view/profile_view.dart
+++ lib/features/home/presentation/view/profile_view.dart
@@ -70,10 +70,25 @@ class _ProfileViewState extends ConsumerState<ProfileView> {
                 }),
                 Space(48.h),
                 MainButton(
-                  loading: false,
+                  loading: ref.watch(logOutProvider).isLoading,
                   text: 'LogOut',
                   pressed: () {
-                    //ref.read(logOutProvider.notifier).signOut(context);
+                    ref.read(logOutProvider.notifier).signOut().then((value) {
+                      if (!ref.watch(logOutProvider).hasError) {
+                        if (context.mounted) {
+                          context.goNamed(AppRoute.login.name);
+                        }
+                      } else {
+                        if (context.mounted) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content:
+                                  Text('${ref.watch(logOutProvider).error}'),
+                            ),
+                          );
+                        }
+                      }
+                    });
                   },
                 ),
               ],
@@ -118,52 +133,3 @@ final List<UserDetails> uD = [
     trailingIcon: AppImage.arrowDown,
   ),
 ];
-
-class DetailsCard extends StatelessWidget {
-  const DetailsCard({
-    required this.model,
-    super.key,
-  });
-  final UserDetails model;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      margin: const EdgeInsets.only(bottom: 10),
-      width: double.maxFinite,
-      height: 54.h,
-      decoration: const BoxDecoration(
-        color: Color(0xff1b1b1b),
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            children: [
-              SvgPicture.asset(
-                model.leadingIcon,
-                width: 20,
-                colorFilter: const ColorFilter.mode(
-                  Colors.white54,
-                  BlendMode.srcIn,
-                ),
-              ),
-              Space(20.w),
-              Text(model.title),
-            ],
-          ),
-          SvgPicture.asset(
-            model.trailingIcon,
-            width: 20,
-            colorFilter: const ColorFilter.mode(
-              Colors.white54,
-              BlendMode.srcIn,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
